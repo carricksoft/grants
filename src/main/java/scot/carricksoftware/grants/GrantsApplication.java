6ff7e5b34536c5870f3c7c8528d92c596b32b0d3
Andrew Grant <carricksoft@gmail.com>
2025-02-02 17:47:15 +0000

    First commit

---
+++ src/main/java/scot/carricksoftware/grants/GrantsApplication.java
@@ -0,0 +1,18 @@
+package scot.carricksoftware.grants;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrantsApplication {
+    private static final Logger logger = LogManager.getLogger(GrantsApplication.class);
+
+    public static void main(String[] args) {
+        logger.debug("GrantsApplication started");
+        SpringApplication.run(GrantsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/scot/carricksoftware/grants/GrantsApplicationTests.java
@@ -0,0 +1,13 @@
+package scot.carricksoftware.grants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
