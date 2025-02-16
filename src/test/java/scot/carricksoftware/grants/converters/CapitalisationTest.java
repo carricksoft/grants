11164d18ce7bfcb5f7f828caaa4170d7775763bc
Andrew Grant <carricksoft@gmail.com>
2025-02-16 20:30:49 +0000

    CapitalisationImpl

--- src/test/java/scot/carricksoftware/grants/converters/CapitalisationTest.java
+++ src/test/java/scot/carricksoftware/grants/converters/CapitalisationTest.java
@@ -23,6 +23,49 @@ class CapitalisationTest {
 
     @Test
     void firstTest() {
+        assertEquals("Smith", capitalisation.getCapitalisation("Smith"));
+    }
+
+    @Test
+    void secondTest() {
         assertEquals("Smith", capitalisation.getCapitalisation("smith"));
     }
+
+    @Test
+    void thirdTest() {
+        assertEquals("Smith", capitalisation.getCapitalisation("SMITH"));
+    }
+
+    @Test
+    void fourthTest() {
+        assertEquals("Smith Jones", capitalisation.getCapitalisation("smith jones"));
+    }
+
+    @Test
+    void fifthTest() {
+        assertEquals("Smith-Jones", capitalisation.getCapitalisation("smith-jones"));
+    }
+
+    @Test
+    void sixthTest() {
+        assertEquals("McSmith", capitalisation.getCapitalisation("mcSmith"));
+    }
+
+    @Test
+    void seventhTest() {
+        //noinspection SpellCheckingInspection
+        assertEquals("Macsmith", capitalisation.getCapitalisation("macsmith"));
+    }
+
+    @Test
+    void eighthTest() {
+        assertEquals("MacSmith", capitalisation.getCapitalisation("macSmith"));
+    }
+
+    @Test
+    void ninthTest() {
+        //noinspection SpellCheckingInspection
+        assertEquals("McSmith", capitalisation.getCapitalisation("mcsmith"));
+    }
+
 }
