fbd0bdac84863a24df905c17d2bf3e3fc2062c3d
Andrew Grant <carricksoft@gmail.com>
2025-02-21 14:41:22 +0000

    Census Command

---
+++ src/main/java/scot/carricksoftware/grants/commands/census/CensusCommand.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c)  21 Feb 2025, Andrew Grant of Carrick Software .
+ * All rights reserved.
+ */
+
+package scot.carricksoftware.grants.commands.census;
+
+import scot.carricksoftware.grants.domains.places.Place;
+
+import java.time.LocalDate;
+
+public class CensusCommand {
+    Long id;
+    Place place;
+    LocalDate date;
+
+    public CensusCommand() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
---
+++ src/test/java/scot/carricksoftware/grants/commands/census/CensusCommandTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c)  21 Feb 2025, Andrew Grant of Carrick Software .
+ * All rights reserved.
+ */
+
+package scot.carricksoftware.grants.commands.census;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import scot.carricksoftware.grants.domains.places.Place;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static scot.carricksoftware.grants.GenerateRandomValues.GetRandomLong;
+import static scot.carricksoftware.grants.GenerateRandomValues.GetRandomPlace;
+
+class CensusCommandTest {
+
+    private CensusCommand censusCommand;
+
+    @BeforeEach
+    void setUp() {
+        censusCommand = new CensusCommand();
+    }
+
+    @Test
+    void getId() {
+        assertNull(censusCommand.getId());
+    }
+
+    @Test
+    void setId() {
+        Long id = GetRandomLong();
+        censusCommand.setId(id);
+        assertEquals(id,censusCommand.getId());
+    }
+
+    @Test
+    void getPlace() {
+        assertNull(censusCommand.getPlace());
+    }
+
+    @Test
+    void setPlace() {
+        Place place = GetRandomPlace();
+        censusCommand.setPlace(place);
+        assertEquals(place,censusCommand.getPlace());
+    }
+
+    @Test
+    void getDate() {
+        assertNull(censusCommand.getDate());
+    }
+
+    @Test
+    void setDate() {
+        LocalDate date = LocalDate.now();
+        censusCommand.setDate(date);
+        assertEquals(date,censusCommand.getDate());
+    }
+}
