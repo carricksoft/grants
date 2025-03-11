2a7c0dce8da304f20ff9d1d8437af5dd7684e7c1
Andrew Grant <carricksoft@gmail.com>
2025-03-11 14:17:48 +0000

    PlaceTest

--- src/main/java/scot/carricksoftware/grants/domains/places/Place.java
+++ src/main/java/scot/carricksoftware/grants/domains/places/Place.java
@@ -7,26 +7,12 @@ package scot.carricksoftware.grants.domains.places;
 
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToOne;
 import scot.carricksoftware.grants.BaseEntity;
 
 @Entity
 public class Place extends BaseEntity {
 
     private String name;
-
-    @OneToOne
-    private Region region;
-
-    @SuppressWarnings("unused")
-    public Region getRegion() {
-        return region;
-    }
-
-    @SuppressWarnings("unused")
-    public void setRegion(Region region) {
-        this.region = region;
-    }
 
     public String getName() {
         return name;
