56da1eb3b3dfff936d5dd6b69b55f686f6464280
Andrew Grant <carricksoft@gmail.com>
2025-03-11 19:03:43 +0000

    Country Converters

---
+++ src/main/java/scot/carricksoftware/grants/converters/places/countries/CountryCommandConverter.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Andrew Grant of Carrick Software 11/03/2025, 18:55. All rights reserved.
+ *
+ */
+
+package scot.carricksoftware.grants.converters.places.countries;
+
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.core.convert.converter.Converter;
+import scot.carricksoftware.grants.commands.places.countries.CountryCommand;
+import scot.carricksoftware.grants.domains.places.Country;
+
+@SuppressWarnings("unused")
+public interface CountryCommandConverter extends Converter<CountryCommand, Country> {
+
+    @Override
+    Country convert(@SuppressWarnings("NullableProblems") @NotNull CountryCommand source);
+
+
+}
