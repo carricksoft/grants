332ad8a87d026da98c30b10d6d9701d1821057c4
Andrew Grant <carricksoft@gmail.com>
2025-03-17 23:05:38 +0000

    Places converters updated (2)

--- src/test/java/scot/carricksoftware/grants/converters/places/regions/RegionConverterImplTest.java
+++ src/test/java/scot/carricksoftware/grants/converters/places/regions/RegionConverterImplTest.java
@@ -11,12 +11,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Component;
 import scot.carricksoftware.grants.commands.places.regions.RegionCommand;
 import scot.carricksoftware.grants.domains.places.Country;
+import scot.carricksoftware.grants.domains.places.Place;
 import scot.carricksoftware.grants.domains.places.Region;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static scot.carricksoftware.grants.GenerateCertificateRandomValues.GetRandomString;
 import static scot.carricksoftware.grants.GenerateRandomNumberValues.GetRandomLong;
 import static scot.carricksoftware.grants.GenerateRandomPlaceValues.GetRandomCountry;
+import static scot.carricksoftware.grants.GenerateRandomPlaceValues.GetRandomPlace;
 
 
 @Component
@@ -35,16 +40,20 @@ public class RegionConverterImplTest {
         Long id = GetRandomLong();
         String name = GetRandomString();
         Country country = GetRandomCountry();
+        List<Place> places = new ArrayList<>();
+        places.add(GetRandomPlace());
 
         source.setId(id);
         source.setName(name);
         source.setCountry(country);
+        source.setPlaces(places);
 
         RegionCommand target = converter.convert(source);
         assert target != null;
         assertEquals(id, target.getId());
         assertEquals(name, target.getName());
         assertEquals(country, target.getCountry());
+        assertEquals(places, target.getPlaces());
     }
 
 }
