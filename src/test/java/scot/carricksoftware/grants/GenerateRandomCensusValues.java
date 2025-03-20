d71541089f0cbee2e000d216f3053f81b51a9bf8
Andrew Grant <carricksoft@gmail.com>
2025-03-20 19:05:01 +0000

    CensusEntryService Tests

--- src/test/java/scot/carricksoftware/grants/GenerateRandomCensusValues.java
+++ src/test/java/scot/carricksoftware/grants/GenerateRandomCensusValues.java
@@ -8,10 +8,14 @@ package scot.carricksoftware.grants;
 import org.springframework.stereotype.Component;
 import scot.carricksoftware.grants.commands.census.CensusCommand;
 import scot.carricksoftware.grants.commands.census.CensusCommandImpl;
+import scot.carricksoftware.grants.commands.census.CensusEntryCommand;
+import scot.carricksoftware.grants.commands.census.CensusEntryCommandImpl;
 import scot.carricksoftware.grants.domains.census.Census;
+import scot.carricksoftware.grants.domains.census.CensusEntry;
 
 import java.time.LocalDate;
 
+import static scot.carricksoftware.grants.GenerateCertificateRandomValues.GetRandomString;
 import static scot.carricksoftware.grants.GenerateRandomNumberValues.GetRandomLong;
 
 @SuppressWarnings("unused")
@@ -31,6 +35,20 @@ public class GenerateRandomCensusValues {
         censusCommand.setId(GetRandomLong());
         censusCommand.setDate(LocalDate.now());
         return censusCommand;
+    }
+
+    public static CensusEntry GetRandomCensusEntry() {
+        CensusEntry censusEntry = new CensusEntry();
+        censusEntry.setId(GetRandomLong());
+        censusEntry.setName(GetRandomString());
+        return censusEntry;
+    }
+
+    public static CensusEntryCommand GetRandomCensusEntryCommand() {
+        CensusEntryCommand censusEntryCommand = new CensusEntryCommandImpl();
+        censusEntryCommand.setId(GetRandomLong());
+        censusEntryCommand.setName(GetRandomString());
+        return censusEntryCommand;
     }
 
 }
