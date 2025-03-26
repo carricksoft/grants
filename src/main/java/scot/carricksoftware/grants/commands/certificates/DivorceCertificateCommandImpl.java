932b5c55bd1dca87c9aacdb7b0bbd775e33b9d89
Andrew Grant <carricksoft@gmail.com>
2025-03-26 10:25:32 +0000

    Command: Marriage Certificate  DivorceCertificate

---
+++ src/main/java/scot/carricksoftware/grants/commands/certificates/DivorceCertificateCommandImpl.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Andrew Grant of Carrick Software 18/03/2025, 02:05. All rights reserved.
+ *
+ */
+
+package scot.carricksoftware.grants.commands.certificates;
+
+public class DivorceCertificateCommandImpl implements DivorceCertificateCommand {
+
+    Long Id;
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+
+}
---
+++ src/main/java/scot/carricksoftware/grants/commands/certificates/MarriageCertificateCommand.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Andrew Grant of Carrick Software 18/03/2025, 02:05. All rights reserved.
+ *
+ */
+
+package scot.carricksoftware.grants.commands.certificates;
+
+public interface MarriageCertificateCommand {
+
+
+    Long getId();
+
+    void setId(Long id);
+
+
+}
