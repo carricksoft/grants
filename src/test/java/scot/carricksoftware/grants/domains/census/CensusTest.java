3350240c0801566c9f779169da49b9029367c08a
Andrew Grant <carricksoft@gmail.com>
2025-04-19 23:48:14 +0100

    Census date changed to an enum

--- src/test/java/scot/carricksoftware/grants/domains/census/CensusTest.java
+++ src/test/java/scot/carricksoftware/grants/domains/census/CensusTest.java
@@ -8,6 +8,7 @@ package scot.carricksoftware.grants.domains.census;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import scot.carricksoftware.grants.domains.places.Place;
 import scot.carricksoftware.grants.enums.census.CensusDate;
@@ -16,7 +17,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static scot.carricksoftware.grants.GenerateCensusRandomEnums.GetRandomCensusDate;
+import static scot.carricksoftware.grants.GenerateCertificateRandomValues.GetRandomString;
 import static scot.carricksoftware.grants.GenerateRandomCensusValues.GetRandomCensusEntry;
 import static scot.carricksoftware.grants.GenerateRandomNumberValues.GetRandomLong;
 import static scot.carricksoftware.grants.GenerateRandomPlaceValues.GetRandomPlace;
@@ -30,6 +33,10 @@ public class CensusTest {
     public void setUp() {
         census = new Census();
     }
+
+    @Mock
+    private Place placeMock;
+
 
     @Test
     public void getIdTest() {
@@ -56,7 +63,6 @@ public class CensusTest {
         assertEquals(censusEntries, census.getCensusEntries());
     }
 
-
     @Test
     public void getPlaceTest() {
         assertNull(census.getPlace());
@@ -72,9 +78,11 @@ public class CensusTest {
     @Test
     public void toStringTest() {
         CensusDate date = GetRandomCensusDate();
+        String placeString =  GetRandomString();
+        when(placeMock.toString()).thenReturn(placeString);
         census.setDate(date);
-        assertEquals(date, census.getDate());
+        census.setPlace(placeMock);
+        assertEquals(placeString + ", " + date.label, census.toString());
     }
-
 
 }
