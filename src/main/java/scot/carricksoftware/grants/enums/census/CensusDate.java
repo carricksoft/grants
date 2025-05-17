2be75110a7e481b75ddcfe047a0acf69d8ed8f1d
Andrew Grant <carricksoft@gmail.com>
2025-05-17 10:26:17 +0100

    All census dates added from 1861 to 1921 and 1939

--- src/main/java/scot/carricksoftware/grants/enums/census/CensusDate.java
+++ src/main/java/scot/carricksoftware/grants/enums/census/CensusDate.java
@@ -7,9 +7,14 @@ package scot.carricksoftware.grants.enums.census;
 
 public enum CensusDate {
 
+    CENSUS_1861("07/04/1861"),
+    CENSUS_1871("02/04/1871"),
     CENSUS_1881("03/04/1881"),
+    CENSUS_1891("05/04/1891"),
     CENSUS_1901("31/03/1901"),
     CENSUS_1911("02/04/1911"),
+    CENSUS_1921("19/06/1921"),
+    CENSUS_1931("26/04/1931"),
     CENSUS_1939("02/09/1939");
 
 
