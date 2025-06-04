d164fe230182306b124cba30ec9d51ba6377e0a1
Andrew Grant <carricksoft@gmail.com>
2025-06-04 01:56:10 +0100

    BirthCertificateCommandPartOneValidatorTest::getCertificateNumber() and ::getCertificateSource

--- src/test/java/scot/carricksoftware/grants/validators/certificates/birthcertificate/BirthCertificateCommandPartOneValidatorTest.java
+++ src/test/java/scot/carricksoftware/grants/validators/certificates/birthcertificate/BirthCertificateCommandPartOneValidatorTest.java
@@ -13,10 +13,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.validation.BindingResult;
 import scot.carricksoftware.grants.commands.certificates.birthcertificates.BirthCertificateCommand;
 import scot.carricksoftware.grants.domains.people.Person;
+import scot.carricksoftware.grants.domains.places.Organisation;
 import scot.carricksoftware.grants.validators.helpers.ValidateTypes;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static scot.carricksoftware.grants.GenerateCertificateRandomValues.GetRandomString;
 
 @ExtendWith(MockitoExtension.class)
 class BirthCertificateCommandPartOneValidatorTest {
@@ -42,5 +44,20 @@ class BirthCertificateCommandPartOneValidatorTest {
         when(birthCertificateCommandMock.getNewBorn()).thenReturn(new Person());
         validator.validate(birthCertificateCommandMock, bindingResultMock);
         verify(validateTypesMock).validatePerson(birthCertificateCommandMock.getNewBorn(), "newBorn", "The New Born cannot be null.", bindingResultMock);
+    }
+
+    @Test
+    void validateCertificateNumberTest() {
+        String string = GetRandomString();
+        when(birthCertificateCommandMock.getCertificateNumber()).thenReturn(string);
+        validator.validate(birthCertificateCommandMock, bindingResultMock);
+        verify(validateTypesMock).validateNullOrEmptyString(string, "certificateNumber", "The certificate number cannot be null.", bindingResultMock);
+    }
+
+    @Test
+    void validateCertificateSourceTest() {
+        when(birthCertificateCommandMock.getCertificateSource()).thenReturn(new Organisation());
+        validator.validate(birthCertificateCommandMock, bindingResultMock);
+        verify(validateTypesMock).validateOrganisation(birthCertificateCommandMock.getCertificateSource(), "certificateSource", "The certificate source cannot be null.", bindingResultMock);
     }
 }
