ae13985d6309c4d3826452299c489850c47be0dd
Andrew Grant <carricksoft@gmail.com>
2025-06-04 01:26:00 +0100

    BirthCertificateCommandPartOneValidatorTest::getNewBorn();

--- src/test/java/scot/carricksoftware/grants/validators/certificates/birthcertificate/BirthCertificateCommandPartOneValidatorTest.java
+++ src/test/java/scot/carricksoftware/grants/validators/certificates/birthcertificate/BirthCertificateCommandPartOneValidatorTest.java
@@ -10,9 +10,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.validation.BindingResult;
+import scot.carricksoftware.grants.commands.certificates.birthcertificates.BirthCertificateCommand;
+import scot.carricksoftware.grants.domains.people.Person;
 import scot.carricksoftware.grants.validators.helpers.ValidateTypes;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class BirthCertificateCommandPartOneValidatorTest {
@@ -22,13 +26,21 @@ class BirthCertificateCommandPartOneValidatorTest {
     @Mock
     private ValidateTypes validateTypesMock;
 
+    @Mock()
+    BirthCertificateCommand birthCertificateCommandMock;
+
+    @Mock
+    private BindingResult bindingResultMock;
+
     @BeforeEach
     void setUp() {
         validator = new BirthCertificateCommandPartOneValidatorImpl(validateTypesMock);
     }
 
     @Test
-    void constructorTest() {
-        assertNotNull(validator);
+    void validateNewBornTest() {
+        when(birthCertificateCommandMock.getNewBorn()).thenReturn(new Person());
+        validator.validate(birthCertificateCommandMock, bindingResultMock);
+        verify(validateTypesMock).validatePerson(birthCertificateCommandMock.getNewBorn(), "newBorn", "The New Born cannot be null.", bindingResultMock);
     }
 }
