557cae153a31f3040686807a290e72b030c9892f
Andrew Grant <carricksoft@gmail.com>
2025-06-24 22:34:19 +0100

    CapitaliseCountry Test

--- src/main/java/scot/carricksoftware/grants/commands/places/countries/CountryCommandImpl.java
+++ src/main/java/scot/carricksoftware/grants/commands/places/countries/CountryCommandImpl.java
@@ -20,6 +20,9 @@ public class CountryCommandImpl implements CountryCommand {
 
     private List<Region> regions;
 
+    public CountryCommandImpl() {
+    }
+
     @Override
     public Long getId() {
         return id;
---
+++ src/test/java/scot/carricksoftware/grants/capitalisation/places/countries/CapitaliseCountryTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2025.  Andrew Grant Carrick Software. All rights reserved
+ *
+ */
+
+package scot.carricksoftware.grants.capitalisation.places.countries;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import scot.carricksoftware.grants.capitalisation.CapitaliseString;
+import scot.carricksoftware.grants.commands.places.countries.CountryCommand;
+import scot.carricksoftware.grants.commands.places.countries.CountryCommandImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CapitaliseCountryTest {
+
+    private CapitaliseCountry capitaliseCountry;
+
+    @Mock
+    private CapitaliseString capitaliseStringMock;
+
+    @BeforeEach
+    void setUp() {
+        capitaliseCountry = new CapitaliseCountryImpl(capitaliseStringMock);
+    }
+
+    @Test
+    void capitaliseNameTest() {
+        CountryCommand countryCommand = new CountryCommandImpl();
+        countryCommand.setName("lower");
+        when(capitaliseStringMock.capitalise("lower")).thenReturn("Lower");
+        capitaliseCountry.capitalise(countryCommand);
+        assertEquals("Lower",countryCommand.getName());
+    }
+}
