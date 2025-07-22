6c88a5677e1d866545b791989481b1ea6dfef52c
Andrew Grant <carricksoft@gmail.com>
2025-07-22 01:17:27 +0100

    DeathCertificateUntrackedFieldsValidatorTest (3)

--- src/test/java/scot/carricksoftware/grants/validators/certificates/deathcertificate/DeathCertificateUntrackedFieldsValidatorTest.java
+++ src/test/java/scot/carricksoftware/grants/validators/certificates/deathcertificate/DeathCertificateUntrackedFieldsValidatorTest.java
@@ -41,7 +41,12 @@ class DeathCertificateUntrackedFieldsValidatorTest {
     private String untrackedFather;
     private Place whereDied;
     private String untrackedWhereDied;
-
+    private Place usualResidence;
+    private String untrackedUsualResidence;
+    private Person mother;
+    private String untrackedMother;
+    private Person informant;
+    private String untrackedInformant;
 
     @BeforeEach
     void setUp() {
@@ -50,17 +55,29 @@ class DeathCertificateUntrackedFieldsValidatorTest {
         spouse = GetRandomPerson();
         father = GetRandomPerson();
         whereDied = GetRandomPlace();
+        usualResidence = GetRandomPlace();
         untrackedSpouse = GetRandomString();
         untrackedFather = GetRandomString();
         untrackedWhereDied = GetRandomString();
+        untrackedUsualResidence = GetRandomString();
+        mother = GetRandomPerson();
+        untrackedMother = GetRandomString();
+        informant = GetRandomPerson();
+        untrackedInformant = GetRandomString();
 
         deathCertificateCommand.setSpouse(spouse);
         deathCertificateCommand.setUntrackedSpouse(untrackedSpouse);
         deathCertificateCommand.setFather(father);
         deathCertificateCommand.setUntrackedFather(untrackedFather);
+        deathCertificateCommand.setMother(mother);
+        deathCertificateCommand.setUntrackedMother(untrackedMother);
+        deathCertificateCommand.setInformant(informant);
+        deathCertificateCommand.setUntrackedInformant(untrackedInformant);
 
         deathCertificateCommand.setWhereDied(whereDied);
         deathCertificateCommand.setUntrackedWhereDied(untrackedWhereDied);
+        deathCertificateCommand.setUsualResidence(usualResidence);
+        deathCertificateCommand.setUntrackedUsualResidence(untrackedUsualResidence);
     }
 
     @Test
@@ -72,6 +89,12 @@ class DeathCertificateUntrackedFieldsValidatorTest {
                 "One and only one father and untracked father must be specified.", bindingResultMock);
         verify(validateTwoFieldTypesMock).validatePlaceAndUntrackedPlace(whereDied, untrackedWhereDied, "whereDied", "untrackedWhereDied",
                 "One and only one where died and untracked where died must be specified.", bindingResultMock);
+        verify(validateTwoFieldTypesMock).validateOptionalPlaceAndUntrackedPlace(usualResidence, untrackedUsualResidence, "usualResidence", "untrackedUsualResidence",
+                "Only one usual residence and untracked usual residence may be specified.", bindingResultMock);
+        verify(validateTwoFieldTypesMock).validatePersonAndUntrackedPerson(mother, untrackedMother, "mother", "untrackedMother",
+                "One and only one mother and untracked mother may be specified.", bindingResultMock);
+        verify(validateTwoFieldTypesMock).validatePersonAndUntrackedPerson(informant, untrackedInformant, "informant", "untrackedInformant",
+                "One and only one informant and untracked informant may be specified.", bindingResultMock);
     }
 
 
