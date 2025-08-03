66ac19a8c0da17215159a6fc3ba08eede03c2190
Andrew Grant <carricksoft@gmail.com>
2025-08-03 23:14:17 +0100

    Command MarriageCertificate

--- src/main/java/scot/carricksoftware/grants/commands/certificates/marriagecertificates/MarriageCertificateCommand.java
+++ src/main/java/scot/carricksoftware/grants/commands/certificates/marriagecertificates/MarriageCertificateCommand.java
@@ -6,6 +6,7 @@
 package scot.carricksoftware.grants.commands.certificates.marriagecertificates;
 
 import scot.carricksoftware.grants.domains.people.Person;
+import scot.carricksoftware.grants.domains.places.Place;
 
 public interface MarriageCertificateCommand {
 
@@ -22,4 +23,92 @@ public interface MarriageCertificateCommand {
     Person getGroom();
 
     void setGroom(Person groom);
+
+    String getWhenMarried();
+
+    void setWhenMarried(String whenMarried);
+
+    Place getWhereMarried();
+
+    void setWhereMarried(Place whereMarried);
+
+    String getUntrackedWhereMarried();
+
+    void setUntrackedWhereMarried(String untrackedWhereMarried);
+
+    String getGroomAge();
+
+    void setGroomAge(String groomAge);
+
+    String getBrideAge();
+
+    void setBrideAge(String brideAge);
+
+    String getGroomCondition();
+
+    void setGroomCondition(String groomCondition);
+
+    String getBrideCondition();
+
+    void setBrideCondition(String brideCondition);
+
+    String getGroomRank();
+
+    void setGroomRank(String groomRank);
+
+    String getBrideRank();
+
+    void setBrideRank(String brideRank);
+
+    Place getGroomUsualResidence();
+
+    void setGroomUsualResidence(Place groomUsualResidence);
+
+    Place getBrideUsualResidence();
+
+    void setBrideUsualResidence(Place brideUsualResidence);
+
+    String getGroomUntrackedResidence();
+
+    void setGroomUntrackedResidence(String groomUntrackedResidence);
+
+    String getBrideUntrackedResidence();
+
+    void setBrideUntrackedResidence(String brideUntrackedResidence);
+
+    Person getGroomFather();
+
+    void setGroomFather(Person groomFather);
+
+    Person getBrideFather();
+
+    void setBrideFather(Person brideFather);
+
+    String getGroomUntrackedFather();
+
+    void setGroomUntrackedFather(String groomUntrackedFather);
+
+    String getBrideUntrackedFather();
+
+    void setBrideUntrackedFather(String brideUntrackedFather);
+
+    String getBrideFatherRank();
+
+    void setBrideFatherRank(String brideFatherRank);
+
+    Person getFirstWitness();
+
+    void setFirstWitness(Person firstWitness);
+
+    Person getSecondWitness();
+
+    void setSecondWitness(Person secondWitness);
+
+    String getUntrackedFirstWitness();
+
+    void setUntrackedFirstWitness(String untrackedFirstWitness);
+
+    String getUntrackedSecondWitness();
+
+    void setUntrackedSecondWitness(String untrackedSecondWitness);
 }
--- src/main/java/scot/carricksoftware/grants/commands/certificates/marriagecertificates/MarriageCertificateCommandImpl.java
+++ src/main/java/scot/carricksoftware/grants/commands/certificates/marriagecertificates/MarriageCertificateCommandImpl.java
@@ -6,14 +6,36 @@
 package scot.carricksoftware.grants.commands.certificates.marriagecertificates;
 
 import scot.carricksoftware.grants.domains.people.Person;
+import scot.carricksoftware.grants.domains.places.Place;
 
 public class MarriageCertificateCommandImpl implements MarriageCertificateCommand {
 
-    Long Id;
+    private Long Id;
+    private Person bride;
+    private Person groom;
+    private String whenMarried;
+    private Place whereMarried;
+    private String untrackedWhereMarried;
+    private String groomAge;
+    private String brideAge;
+    private String groomCondition;
+    private String brideCondition;
+    private String groomRank;
+    private String brideRank;
+    private Place groomUsualResidence;
+    private Place brideUsualResidence;
+    private String groomUntrackedResidence;
+    private String brideUntrackedResidence;
+    private Person groomFather;
+    private Person brideFather;
+    private String groomUntrackedFather;
+    private String brideUntrackedFather;
+    private String brideFatherRank;
+    private Person firstWitness;
+    private Person secondWitness;
+    private String untrackedFirstWitness;
+    private String untrackedSecondWitness;
 
-    Person bride;
-
-    Person groom;
 
     @Override
     public Long getId() {
@@ -24,27 +46,243 @@ public class MarriageCertificateCommandImpl implements MarriageCertificateComman
         Id = id;
     }
 
-
     @Override
     public Person getBride() {
         return bride;
     }
-
 
     @Override
     public void setBride(Person bride) {
         this.bride = bride;
     }
 
-
     @Override
     public Person getGroom() {
         return groom;
     }
 
-
     @Override
     public void setGroom(Person groom) {
         this.groom = groom;
+    }
+
+    @Override
+    public String getWhenMarried() {
+        return whenMarried;
+    }
+
+    @Override
+    public void setWhenMarried(String whenMarried) {
+        this.whenMarried = whenMarried;
+    }
+
+    @Override
+    public Place getWhereMarried() {
+        return whereMarried;
+    }
+
+    @Override
+    public void setWhereMarried(Place whereMarried) {
+        this.whereMarried = whereMarried;
+    }
+
+    @Override
+    public String getUntrackedWhereMarried() {
+        return untrackedWhereMarried;
+    }
+
+    @Override
+    public void setUntrackedWhereMarried(String untrackedWhereMarried) {
+        this.untrackedWhereMarried = untrackedWhereMarried;
+    }
+
+    @Override
+    public String getGroomAge() {
+        return groomAge;
+    }
+
+    @Override
+    public void setGroomAge(String groomAge) {
+        this.groomAge = groomAge;
+    }
+
+    @Override
+    public String getBrideAge() {
+        return brideAge;
+    }
+
+    @Override
+    public void setBrideAge(String brideAge) {
+        this.brideAge = brideAge;
+    }
+
+    @Override
+    public String getGroomCondition() {
+        return groomCondition;
+    }
+
+    @Override
+    public void setGroomCondition(String groomCondition) {
+        this.groomCondition = groomCondition;
+    }
+
+    @Override
+    public String getBrideCondition() {
+        return brideCondition;
+    }
+
+    @Override
+    public void setBrideCondition(String brideCondition) {
+        this.brideCondition = brideCondition;
+    }
+
+    @Override
+    public String getGroomRank() {
+        return groomRank;
+    }
+
+    @Override
+    public void setGroomRank(String groomRank) {
+        this.groomRank = groomRank;
+    }
+
+    @Override
+    public String getBrideRank() {
+        return brideRank;
+    }
+
+    @Override
+    public void setBrideRank(String brideRank) {
+        this.brideRank = brideRank;
+    }
+
+    @Override
+    public Place getGroomUsualResidence() {
+        return groomUsualResidence;
+    }
+
+    @Override
+    public void setGroomUsualResidence(Place groomUsualResidence) {
+        this.groomUsualResidence = groomUsualResidence;
+    }
+
+    @Override
+    public Place getBrideUsualResidence() {
+        return brideUsualResidence;
+    }
+
+    @Override
+    public void setBrideUsualResidence(Place brideUsualResidence) {
+        this.brideUsualResidence = brideUsualResidence;
+    }
+
+    @Override
+    public String getGroomUntrackedResidence() {
+        return groomUntrackedResidence;
+    }
+
+    @Override
+    public void setGroomUntrackedResidence(String groomUntrackedResidence) {
+        this.groomUntrackedResidence = groomUntrackedResidence;
+    }
+
+    @Override
+    public String getBrideUntrackedResidence() {
+        return brideUntrackedResidence;
+    }
+
+    @Override
+    public void setBrideUntrackedResidence(String brideUntrackedResidence) {
+        this.brideUntrackedResidence = brideUntrackedResidence;
+    }
+
+    @Override
+    public Person getGroomFather() {
+        return groomFather;
+    }
+
+    @Override
+    public void setGroomFather(Person groomFather) {
+        this.groomFather = groomFather;
+    }
+
+    @Override
+    public Person getBrideFather() {
+        return brideFather;
+    }
+
+    @Override
+    public void setBrideFather(Person brideFather) {
+        this.brideFather = brideFather;
+    }
+
+    @Override
+    public String getGroomUntrackedFather() {
+        return groomUntrackedFather;
+    }
+
+    @Override
+    public void setGroomUntrackedFather(String groomUntrackedFather) {
+        this.groomUntrackedFather = groomUntrackedFather;
+    }
+
+    @Override
+    public String getBrideUntrackedFather() {
+        return brideUntrackedFather;
+    }
+
+    @Override
+    public void setBrideUntrackedFather(String brideUntrackedFather) {
+        this.brideUntrackedFather = brideUntrackedFather;
+    }
+
+    @Override
+    public String getBrideFatherRank() {
+        return brideFatherRank;
+    }
+
+    @Override
+    public void setBrideFatherRank(String brideFatherRank) {
+        this.brideFatherRank = brideFatherRank;
+    }
+
+    @Override
+    public Person getFirstWitness() {
+        return firstWitness;
+    }
+
+    @Override
+    public void setFirstWitness(Person firstWitness) {
+        this.firstWitness = firstWitness;
+    }
+
+    @Override
+    public Person getSecondWitness() {
+        return secondWitness;
+    }
+
+    @Override
+    public void setSecondWitness(Person secondWitness) {
+        this.secondWitness = secondWitness;
+    }
+
+    @Override
+    public String getUntrackedFirstWitness() {
+        return untrackedFirstWitness;
+    }
+
+    @Override
+    public void setUntrackedFirstWitness(String untrackedFirstWitness) {
+        this.untrackedFirstWitness = untrackedFirstWitness;
+    }
+
+    @Override
+    public String getUntrackedSecondWitness() {
+        return untrackedSecondWitness;
+    }
+
+    @Override
+    public void setUntrackedSecondWitness(String untrackedSecondWitness) {
+        this.untrackedSecondWitness = untrackedSecondWitness;
     }
 }
