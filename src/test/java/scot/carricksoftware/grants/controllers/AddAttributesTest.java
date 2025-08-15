5200b5aefc4ab97f25f8b8ca4ab9ca3548916614
Andrew Grant <carricksoft@gmail.com>
2025-08-15 17:43:23 +0100

    AddAttributesTest created

--- src/test/java/scot/carricksoftware/grants/controllers/AddAttributesTest.java
+++ src/test/java/scot/carricksoftware/grants/controllers/AddAttributesTest.java
@@ -6,10 +6,34 @@
 package scot.carricksoftware.grants.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import scot.carricksoftware.grants.services.people.PersonService;
+import scot.carricksoftware.grants.services.places.organisations.OrganisationService;
+import scot.carricksoftware.grants.services.places.places.PlaceService;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class AddAttributesTest {
 
+    private AddAttributes addAttributes;
+
+    @Mock
+    private PersonService personServiceMock;
+
+    @Mock
+    private PlaceService placeServiceMock;
+
+    @Mock
+    private OrganisationService organisationServiceMock;
+
     @BeforeEach
     void setUp() {
+        addAttributes = new AddAttributesImpl(personServiceMock, placeServiceMock, organisationServiceMock);
+    }
+
+    @Test
+    void constructorTest() {
+        assertNotNull(addAttributes);
     }
 }
