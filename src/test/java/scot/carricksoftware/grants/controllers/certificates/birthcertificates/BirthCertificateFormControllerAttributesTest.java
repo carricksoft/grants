8a392382ad15804e24430fb092854809712ad154
Andrew Grant <carricksoft@gmail.com>
2025-08-16 08:58:18 +0100

    AddAttributes Test BirthCertificate

--- src/test/java/scot/carricksoftware/grants/controllers/certificates/birthcertificates/BirthCertificateFormControllerAttributesTest.java
+++ src/test/java/scot/carricksoftware/grants/controllers/certificates/birthcertificates/BirthCertificateFormControllerAttributesTest.java
@@ -13,26 +13,19 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.Model;
 import scot.carricksoftware.grants.capitalisation.certificates.birthcertificates.CapitaliseBirthCertificate;
-import scot.carricksoftware.grants.commands.certificates.birthcertificates.BirthCertificateCommand;
-import scot.carricksoftware.grants.constants.AttributeConstants;
 import scot.carricksoftware.grants.controllers.AddAttributes;
 import scot.carricksoftware.grants.converters.certificates.birthcertificates.BirthCertificateCommandConverterImpl;
 import scot.carricksoftware.grants.converters.certificates.birthcertificates.BirthCertificateConverterImpl;
-import scot.carricksoftware.grants.domains.certificates.BirthCertificate;
 import scot.carricksoftware.grants.services.certificates.birthcertificates.BirthCertificateService;
 import scot.carricksoftware.grants.services.certificates.birthcertificates.UpdateCertifiedYearOfBirth;
 import scot.carricksoftware.grants.validators.certificates.birthcertificate.BirthCertificateCommandValidatorImpl;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static scot.carricksoftware.grants.GenerateCertificateRandomValues.GetRandomBirthCertificate;
-import static scot.carricksoftware.grants.GenerateCertificateRandomValues.GetRandomBirthCertificateCommand;
-import static scot.carricksoftware.grants.GenerateRandomNumberValues.GetRandomLong;
+
 
 
 @ExtendWith(MockitoExtension.class)
-public class BirthCertificateFormControllerTest {
+public class BirthCertificateFormControllerAttributesTest {
 
     @SuppressWarnings("unused")
     private BirthCertificateFormControllerImpl birthCertificateFormController;
@@ -75,25 +68,9 @@ public class BirthCertificateFormControllerTest {
 
 
     @Test
-    public void birthCertificateEditTestEditTest() {
-        Long id = GetRandomLong();
-        BirthCertificate birthCertificate = GetRandomBirthCertificate();
-        when(birthCertificateServiceMock.findById(id)).thenReturn(birthCertificate);
-
-        assertEquals("certificates/birthCertificate/form", birthCertificateFormController.birthCertificateEdit(id + "", modelMock));
-        verify(modelMock).addAttribute(AttributeConstants.BIRTH_CERTIFICATE_COMMAND, birthCertificate);
-    }
-
-    @Test
-    public void showByIdTest() {
-        Long id = GetRandomLong();
-        BirthCertificate birthCertificate = GetRandomBirthCertificate();
-        BirthCertificateCommand birthCertificateCommand = GetRandomBirthCertificateCommand();
-
-        when(birthCertificateServiceMock.findById(id)).thenReturn(birthCertificate);
-        when(birthCertificateConverterMock.convert(birthCertificate)).thenReturn(birthCertificateCommand);
-        assertEquals("certificates/birthCertificate/form", birthCertificateFormController.showById(id + "", modelMock));
-        verify(modelMock).addAttribute(AttributeConstants.BIRTH_CERTIFICATE_COMMAND, birthCertificateCommand);
+    public void newCertificateTest() {
+        birthCertificateFormController.getNewBirthCertificate(modelMock);
+        verify(addAttributesMock).AddBMDCertificate(modelMock);
     }
 
 
