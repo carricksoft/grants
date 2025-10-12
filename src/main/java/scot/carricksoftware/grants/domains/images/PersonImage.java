0b51ebafdd845e610189f0cd0e7d887c47a43f26
Andrew Grant <carricksoft@gmail.com>
2025-10-12 16:57:17 +0100

    PersonEdit Domain

--- src/main/java/scot/carricksoftware/grants/domains/images/PersonImage.java
+++ src/main/java/scot/carricksoftware/grants/domains/images/PersonImage.java
@@ -5,6 +5,7 @@
 
 package scot.carricksoftware.grants.domains.images;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -14,10 +15,28 @@ import scot.carricksoftware.grants.domains.people.Person;
 @Entity
 public class PersonImage extends BaseEntity {
 
-    @SuppressWarnings("JpaDataSourceORMInspection")
     @ManyToOne
     @JoinColumn(name = "person_id")
     private Person person;
+
+    @ManyToOne
+    @JoinColumn(name = "`image_id`")
+    private Image image;
+
+    @Column(name = "`level`")
+    private String level;
+
+    @Column(name = "`order`")
+    private String order;
+
+    @Column(name = "`caption`")
+    private String caption;
+
+    @Column(name = "`height`")
+    private String height;
+
+    @Column(name = "`width`")
+    private String width;
 
     public Person getPerson() {
         return person;
@@ -25,5 +44,53 @@ public class PersonImage extends BaseEntity {
 
     public void setPerson(Person person) {
         this.person = person;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public void setWidth(String width) {
+        this.width = width;
     }
 }
