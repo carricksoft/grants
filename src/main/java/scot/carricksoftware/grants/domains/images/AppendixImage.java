a82db4fd95b090ccc9133cce14b8b32ce76f82e3
Andrew Grant <carricksoft@gmail.com>
2025-10-31 22:29:08 +0000

    Add appendixImage to domains

---
+++ src/main/java/scot/carricksoftware/grants/domains/images/AppendixImage.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) Andrew Grant of Carrick Software 18/03/2025, 01:50. All rights reserved.
+ *
+ */
+
+package scot.carricksoftware.grants.domains.images;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import scot.carricksoftware.grants.BaseEntity;
+
+@Entity
+public class AppendixImage extends BaseEntity {
+
+    @SuppressWarnings("JpaDataSourceORMInspection")
+    @ManyToOne
+    @JoinColumn(name = "`image_id`")
+    private Image image;
+
+    @SuppressWarnings("JpaDataSourceORMInspection")
+    @Column(name = "`level`")
+    private String level;
+
+    @SuppressWarnings("JpaDataSourceORMInspection")
+    @Column(name = "`order`")
+    private String order;
+
+    @SuppressWarnings("JpaDataSourceORMInspection")
+    @Column(name = "`caption`")
+    private String caption;
+
+    @SuppressWarnings("JpaDataSourceORMInspection")
+    @Column(name = "`height`")
+    private String height;
+
+    @SuppressWarnings("JpaDataSourceORMInspection")
+    @Column(name = "`width`")
+    private String width;
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public void setWidth(String width) {
+        this.width = width;
+    }
+
+
+}
