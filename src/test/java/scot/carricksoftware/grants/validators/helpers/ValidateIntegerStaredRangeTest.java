5bdac9a69afbd965ebba3b9b5c579d3e9cb2287a
apg <carricksoft@gmail.com>
2025-11-23 10:22:03 +0000

    ValidateIntegerStaredRangeTest

---
+++ src/test/java/scot/carricksoftware/grants/validators/helpers/ValidateIntegerStaredRangeTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2025.  Andrew Grant Carrick Software. All rights reserved
+ *
+ */
+
+package scot.carricksoftware.grants.validators.helpers;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.validation.BindingResult;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static scot.carricksoftware.grants.GenerateCertificateRandomValues.GetRandomString;
+
+@ExtendWith(MockitoExtension.class)
+class ValidateIntegerStaredRangeTest {
+
+    private ValidateTypes validateTypes;
+
+    @Mock
+    private BindingResult bindingResultMock;
+
+    String fieldName;
+
+    @BeforeEach
+    public void setUp() {
+        validateTypes = new ValidateTypesImpl();
+        fieldName = GetRandomString();
+    }
+
+    @Test
+    public void constructorTest() {
+        assertNotNull(validateTypes);
+    }
+
+    @Test
+    public void validateNullInteger() {
+        String nullMessage = GetRandomString();
+
+        validateTypes.validateIntegerStaredRange(null, null, null, fieldName, nullMessage, "", "", bindingResultMock);
+        verify(bindingResultMock).rejectValue(fieldName, "", null, nullMessage);
+    }
+
+    @Test
+    public void validateEmptyInteger() {
+        String nullMessage = GetRandomString();
+
+        validateTypes.validateIntegerStaredRange("*", null, null, fieldName, nullMessage, "", "", bindingResultMock);
+        verify(bindingResultMock).rejectValue(fieldName, "", null, nullMessage);
+    }
+
+    @Test
+    public void validateBadFormatInteger() {
+        String formatMessage = GetRandomString();
+
+        validateTypes.validateIntegerStaredRange("zz", null, null, fieldName, "", formatMessage, "", bindingResultMock);
+        verify(bindingResultMock).rejectValue(fieldName, "", null, formatMessage);
+    }
+
+    @Test
+    public void validateTooLowInteger() {
+        String rangeMessage = GetRandomString();
+
+        validateTypes.validateIntegerStaredRange("-6*", -5, 99, fieldName, "", "", rangeMessage, bindingResultMock);
+        verify(bindingResultMock).rejectValue(fieldName, "", null, rangeMessage);
+    }
+
+    @Test
+    public void validateLowLimitInteger() {
+        validateTypes.validateIntegerStaredRange("-6*", -6, 99, fieldName, "", "", "", bindingResultMock);
+        verifyNoInteractions(bindingResultMock);
+    }
+
+    @Test
+    public void validateHighLimitInteger() {
+        validateTypes.validateIntegerStaredRange("10*", -6, 10, fieldName, "", "", "", bindingResultMock);
+        verifyNoInteractions(bindingResultMock);
+    }
+
+    @Test
+    public void validateTooHighInteger() {
+        String rangeMessage = GetRandomString();
+
+        validateTypes.validateIntegerStaredRange("11*", -5, 10, fieldName, "", "", rangeMessage, bindingResultMock);
+        verify(bindingResultMock).rejectValue(fieldName, "", null, rangeMessage);
+    }
+
+
+}
