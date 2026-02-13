60a884795f31bad2e0562c0449d147496b3ba173
apg <carricksoft@gmail.com>
2026-02-13 11:16:47 +0000

    PlaceImageCommandConverterTest

--- src/test/java/scot/carricksoftware/grants/converters/images/placeImage/PlaceImageCommandConverterTest.java
+++ src/test/java/scot/carricksoftware/grants/converters/images/placeImage/PlaceImageCommandConverterTest.java
@@ -11,11 +11,13 @@ import scot.carricksoftware.grants.commands.images.PlaceImageCommand;
 import scot.carricksoftware.grants.commands.images.PlaceImageCommandImpl;
 import scot.carricksoftware.grants.converters.images.placeimage.PlaceImageCommandConverter;
 import scot.carricksoftware.grants.converters.images.placeimage.PlaceImageCommandConverterImpl;
+import scot.carricksoftware.grants.domains.images.Image;
 import scot.carricksoftware.grants.domains.images.PlaceImage;
 import scot.carricksoftware.grants.domains.places.Place;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static scot.carricksoftware.grants.GenerateCertificateRandomValues.GetRandomString;
 import static scot.carricksoftware.grants.GenerateRandomNumberValues.GetRandomLong;
 import static scot.carricksoftware.grants.GenerateRandomPlaceValues.GetRandomPlace;
 
@@ -32,10 +34,24 @@ class PlaceImageCommandConverterTest {
     void covertTest() {
         Long Id = GetRandomLong();
         Place place = GetRandomPlace();
+        Image image = new Image();
+        String level = GetRandomString();
+        String order = GetRandomString();
+        String caption = GetRandomString();
+        String height = GetRandomString();
+        String width = GetRandomString();
+
+
         PlaceImageCommand source = new PlaceImageCommandImpl();
 
-        source.setId(Id);
         source.setPlace(place);
+        source.setId(Id);
+        source.setImage(image);
+        source.setLevel(level);
+        source.setOrder(order);
+        source.setCaption(caption);
+        source.setHeight(height);
+        source.setWidth(width);
 
 
         PlaceImage target = converter.convert(source);
@@ -43,5 +59,11 @@ class PlaceImageCommandConverterTest {
         assertNotNull(target);
         assertEquals(Id, target.getId());
         assertEquals(place, target.getPlace());
+        assertEquals(image, target.getImage());
+        assertEquals(level, target.getLevel());
+        assertEquals(order, target.getOrder());
+        assertEquals(caption, target.getCaption());
+        assertEquals(height, target.getHeight());
+        assertEquals(width, target.getWidth());
     }
 }
--- src/test/java/scot/carricksoftware/grants/converters/images/placeImage/PlaceImageConverterTest.java
+++ src/test/java/scot/carricksoftware/grants/converters/images/placeImage/PlaceImageConverterTest.java
@@ -10,11 +10,13 @@ import org.junit.jupiter.api.Test;
 import scot.carricksoftware.grants.commands.images.PlaceImageCommand;
 import scot.carricksoftware.grants.converters.images.placeimage.PlaceImageConverter;
 import scot.carricksoftware.grants.converters.images.placeimage.PlaceImageConverterImpl;
+import scot.carricksoftware.grants.domains.images.Image;
 import scot.carricksoftware.grants.domains.images.PlaceImage;
 import scot.carricksoftware.grants.domains.places.Place;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static scot.carricksoftware.grants.GenerateCertificateRandomValues.GetRandomString;
 import static scot.carricksoftware.grants.GenerateRandomNumberValues.GetRandomLong;
 import static scot.carricksoftware.grants.GenerateRandomPlaceValues.GetRandomPlace;
 
@@ -31,16 +33,35 @@ class PlaceImageConverterTest {
     void covertTest() {
         Long Id = GetRandomLong();
         Place place = GetRandomPlace();
+        Image image = new Image();
+        String level = GetRandomString();
+        String order = GetRandomString();
+        String caption = GetRandomString();
+        String height = GetRandomString();
+        String width = GetRandomString();
 
         PlaceImage source = new PlaceImage();
 
         source.setId(Id);
         source.setPlace(place);
+        source.setImage(image);
+        source.setLevel(level);
+        source.setOrder(order);
+        source.setCaption(caption);
+        source.setHeight(height);
+        source.setWidth(width);
 
         PlaceImageCommand target = converter.convert(source);
 
         assertNotNull(target);
         assertEquals(Id, target.getId());
         assertEquals(place, target.getPlace());
+        assertEquals(image, target.getImage());
+        assertEquals(level, target.getLevel());
+        assertEquals(order, target.getOrder());
+        assertEquals(caption, target.getCaption());
+        assertEquals(height, target.getHeight());
+        assertEquals(width, target.getWidth());
+
     }
 }
