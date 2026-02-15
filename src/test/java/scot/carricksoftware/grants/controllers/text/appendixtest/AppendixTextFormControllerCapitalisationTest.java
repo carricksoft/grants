76fcec3258a1ffcbace96edb5de23fe3876d74bd
apg <carricksoft@gmail.com>
2026-02-15 13:29:20 +0000

    AppendixTextFormControllerCapitalisationTest

---
+++ src/test/java/scot/carricksoftware/grants/controllers/text/appendixtest/AppendixTextFormControllerCapitalisationTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) Andrew Grant of Carrick Software 29/03/2025, 13:55. All rights reserved.
+ *
+ */
+
+package scot.carricksoftware.grants.controllers.text.appendixtest;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import scot.carricksoftware.grants.capitalisation.text.appendixText.CapitaliseAppendixTextCommand;
+import scot.carricksoftware.grants.commands.text.AppendixTextCommand;
+import scot.carricksoftware.grants.controllers.text.appendixtext.AppendixTextFormControllerImpl;
+import scot.carricksoftware.grants.converters.text.appendixtext.AppendixTextCommandConverterImpl;
+import scot.carricksoftware.grants.converters.text.appendixtext.AppendixTextConverterImpl;
+import scot.carricksoftware.grants.services.text.appendixtext.AppendixTextService;
+import scot.carricksoftware.grants.validators.text.AppendixTextCommandValidatorImpl;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+public class AppendixTextFormControllerCapitalisationTest {
+
+    @SuppressWarnings("unused")
+    private AppendixTextFormControllerImpl appendixTextController;
+
+    @Mock
+    private AppendixTextService appendixTextServiceMock;
+
+    @Mock
+    private AppendixTextCommandConverterImpl appendixTextCommandConverterMock;
+
+    @Mock
+    private AppendixTextConverterImpl appendixTextConverterMock;
+
+
+    @Mock
+    private AppendixTextCommand appendixTextCommandMock;
+
+    @Mock
+    private BindingResult bindingResultMock;
+
+    @Mock
+    private AppendixTextCommandValidatorImpl appendixTextCommandValidatorImplMock;
+
+    @Mock
+    private CapitaliseAppendixTextCommand capitaliseAppendixTextCommandMock;
+
+    @Mock
+    private Model modelMock;
+
+
+    @BeforeEach
+    public void setUp() {
+        appendixTextController = new AppendixTextFormControllerImpl(appendixTextServiceMock,
+                appendixTextCommandConverterMock,
+                appendixTextConverterMock,
+                appendixTextCommandValidatorImplMock,
+                capitaliseAppendixTextCommandMock);
+    }
+
+
+    @Test
+    public void saveOrUpdateValidationTest() {
+        when(appendixTextServiceMock.saveAppendixTextCommand(any())).thenReturn(appendixTextCommandMock);
+        appendixTextController.saveOrUpdate(appendixTextCommandMock, bindingResultMock, modelMock);
+        verify(capitaliseAppendixTextCommandMock).capitalise(appendixTextCommandMock);
+    }
+
+
+}
